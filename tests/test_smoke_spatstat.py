2e1a0a49492d1d040404a66f01ab8984809574a7
Guillaume Gautier <guillaume.gga@gmail.com>
2021-07-01 12:08:59 +0200

    print ok retry spatstatinterface

--- tests/test_smoke_spatstat.py
+++ tests/test_smoke_spatstat.py
@@ -3,5 +3,6 @@ from spatstat_interface.interface import SpatstatInterface
 
 def test_smoke_spatstat():
     print("smoke-before")
+    spatstat = SpatstatInterface(update=True)
     assert False
     print("smoke-after")
