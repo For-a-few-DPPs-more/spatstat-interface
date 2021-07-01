9840f6b66b52be1db745eb9770debba4b8dc5055
Guillaume Gautier <guillaume.gga@gmail.com>
2021-07-01 12:02:23 +0200

    print doesn't show on CI, force assert False

--- tests/test_smoke_spatstat.py
+++ tests/test_smoke_spatstat.py
@@ -3,5 +3,5 @@ from spatstat_interface.interface import SpatstatInterface
 
 def test_smoke_spatstat():
     print("smoke-before")
-    spatstat = SpatstatInterface(update=False)
+    assert False
     print("smoke-after")
