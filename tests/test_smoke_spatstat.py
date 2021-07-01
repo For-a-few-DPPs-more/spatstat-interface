dbbd5706272f7a92bc0ff9bf9e8553ffbe9466fb
Guillaume Gautier <guillaume.gga@gmail.com>
2021-07-01 11:55:03 +0200

    try to understand CI#4 build error

--- tests/test_smoke_spatstat.py
+++ tests/test_smoke_spatstat.py
@@ -2,4 +2,6 @@ from spatstat_interface.interface import SpatstatInterface
 
 
 def test_smoke_spatstat():
+    print("smoke-before")
     spatstat = SpatstatInterface(update=False)
+    print("smoke-after")
